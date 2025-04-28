d6de7823d3137416d6cb1ab35a57c7ae2a93f9dc
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2025-04-27 21:47:24 -0600

    Reorganiza las rutas de la API para mejorar la estructura y la legibilidad del código.

--- routes/api.php
+++ routes/api.php
@@ -23,40 +23,40 @@ Route::group(['middleware' => [JwtMiddleware::class]], function () {
     //This is an example you ought to erase it and create your own
     Route::get('/users', [UsersController::class, 'getAllUsers'])->name('getAllUsers');
     Route::get('/users/{id}', [UsersController::class, 'getUserById'])->name('getUserById');
-});
 
-Route::prefix('/lotes')->group(function () {
-    Route::get('/info', [LoteController::class, 'getLotesConSalidasYIncidencias']);
-    Route::get('/info/{loteId}', [LoteController::class, 'getLoteConSalidasYIncidencias']);
-    Route::get('', [LoteController::class, 'index']);
-    Route::post('', [LoteController::class, 'store']);
-    Route::get('/{id}', [LoteController::class, 'show']);
-    Route::patch('/{id}', [LoteController::class, 'update']);
-    Route::delete('/{id}', [LoteController::class, 'destroy']);
-});
+    Route::prefix('/lotes')->group(function () {
+        Route::get('/info', [LoteController::class, 'getLotesConSalidasYIncidencias']);
+        Route::get('/info/{loteId}', [LoteController::class, 'getLoteConSalidasYIncidencias']);
+        Route::get('', [LoteController::class, 'index']);
+        Route::post('', [LoteController::class, 'store']);
+        Route::get('/{id}', [LoteController::class, 'show']);
+        Route::patch('/{id}', [LoteController::class, 'update']);
+        Route::delete('/{id}', [LoteController::class, 'destroy']);
+    });
 
-Route::prefix('/salidas')->group(function () {
-    Route::get('', [SalidaController::class, 'index']);
-    Route::get('/{id}', [SalidaController::class, 'show']);
-    Route::get('/lote/{lote_id}', [SalidaController::class, 'getByLoteId']);
-    Route::post('', [SalidaController::class, 'store']);
-    Route::patch('/{id}', [SalidaController::class, 'update']);
-    Route::delete('/{id}', [SalidaController::class, 'destroy']);
-});
+    Route::prefix('/salidas')->group(function () {
+        Route::get('', [SalidaController::class, 'index']);
+        Route::get('/{id}', [SalidaController::class, 'show']);
+        Route::get('/lote/{lote_id}', [SalidaController::class, 'getByLoteId']);
+        Route::post('', [SalidaController::class, 'store']);
+        Route::patch('/{id}', [SalidaController::class, 'update']);
+        Route::delete('/{id}', [SalidaController::class, 'destroy']);
+    });
 
-Route::prefix('/incidencias')->group(function () {
-    Route::get('', [EspecificacionIncidenciaController::class, 'index']);
-    Route::get('/{id}', [EspecificacionIncidenciaController::class, 'show']);
-    Route::get('/lote/{lote_id}', [EspecificacionIncidenciaController::class, 'getByLoteId']);
-    Route::get('/salida/{salida_id}', [EspecificacionIncidenciaController::class, 'getBySalidaId']);
-    Route::post('', [EspecificacionIncidenciaController::class, 'store']);
-    Route::patch('/{id}', [EspecificacionIncidenciaController::class, 'update']);
-    Route::delete('/{id}', [EspecificacionIncidenciaController::class, 'destroy']);
-});
+    Route::prefix('/incidencias')->group(function () {
+        Route::get('', [EspecificacionIncidenciaController::class, 'index']);
+        Route::get('/{id}', [EspecificacionIncidenciaController::class, 'show']);
+        Route::get('/lote/{lote_id}', [EspecificacionIncidenciaController::class, 'getByLoteId']);
+        Route::get('/salida/{salida_id}', [EspecificacionIncidenciaController::class, 'getBySalidaId']);
+        Route::post('', [EspecificacionIncidenciaController::class, 'store']);
+        Route::patch('/{id}', [EspecificacionIncidenciaController::class, 'update']);
+        Route::delete('/{id}', [EspecificacionIncidenciaController::class, 'destroy']);
+    });
 
-Route::prefix('/productos')->group(function () {
-    Route::get('', [ProductoController::class, 'getIdAndNombre']);
-    Route::get('/detalles', [ProductoController::class, 'getIdNombrePresentacionCategoria']);
-    Route::get('/categorias', [ProductoController::class, 'getCategoriasUnicas']);
-    Route::get('/{categoria}', [ProductoController::class, 'getByCategoria']);
+    Route::prefix('/productos')->group(function () {
+        Route::get('', [ProductoController::class, 'getIdAndNombre']);
+        Route::get('/detalles', [ProductoController::class, 'getIdNombrePresentacionCategoria']);
+        Route::get('/categorias', [ProductoController::class, 'getCategoriasUnicas']);
+        Route::get('/{categoria}', [ProductoController::class, 'getByCategoria']);
+    });
 });
